58fc088c7f979ef4f96ffd3b2b270b3af152abfc
Beuhnnyto <adam.malek@efrei.net>
2024-02-08 16:54:10 +0100

    resfgb

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express, { Express, Request, Response , Application } from 'express';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
 //For env File 
-// dotenv.config();
+dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
