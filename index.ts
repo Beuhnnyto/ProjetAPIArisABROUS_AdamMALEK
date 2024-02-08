835dce5b82f6a33c6f6de4395b6adbe89940c7da
Beuhnnyto <adam.malek@efrei.net>
2024-02-08 17:38:54 +0100

    oui

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response , Application } from 'express';
+import axios from 'axios';
 import dotenv from 'dotenv';
 
 //For env File 
@@ -7,10 +8,34 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+
+app.get('/characters', async (req: Request, res: Response) => {
+    try {
+        const response = await axios.get('https://rickandmortyapi.com/api/character');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/locations', async (req: Request, res: Response) => {
+    try {
+        const response = await axios.get('https://rickandmortyapi.com/api/location');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/episodes', async (req: Request, res: Response) => {
+    try {
+        const response = await axios.get('https://rickandmortyapi.com/api/episode');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
